6fd6738279539a73144cbec55a43ac6336b4c4eb
Carlos Estrada <cestr008@gmail.com>
2023-04-13 20:02:20 -0400

    Format template

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import {readFile, writeFile} from 'fs/promises';
+
+let template = await readFile(new URL('./template.html', import.meta.url), 'utf-8');
+
+const data = {
+    title: 'This is node.js',
+    body: 'This is the final HTML' 
+}
+
+for (const [k, v] of Object.entries(data)) {
+    template = template.replace(`{${k}}`, v);
+}
+
+await writeFile(new URL('./index.html', import.meta.url), template);
